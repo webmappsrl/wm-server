41b3faa441e1ea58dd688345b7a4542da80d301a
piccioli <piccioli@netseven.it>
2016-09-27 17:34:05 +0200

    Agiunto il metodo checkPoi() che controlla la corretta struttura necessaria ai Punti di Interesse

--- src/classes/WebmappProjectStructure.php
+++ src/classes/WebmappProjectStructure.php
@@ -16,5 +16,57 @@
 
 
 class WebmappProjectStructure {
-	
+
+	private $root;
+	private $geojson;
+	private $poi;
+	private $poi_single;
+
+	private $all_dir = array();
+
+	public function __construct ( $root ) {
+		$this->root = $root;
+		if(substr($this->root, -1)!='/') $this->root .= '/';
+		$this->geojson = $this->root.'geojson/';
+		$this->poi = $this->geojson.'poi/';
+		$this->poi_single = $this->poi.'id/';
+
+		$this->all_dir = 
+		  array(
+		  	$this->root,
+		  	$this->geojson,
+		  	$this->poi,
+		  	$this->poi_single
+		  	);
+	}
+
+	public function getRoot() { return $this->root; }
+	public function getGeojson() { return $this->geojson; }
+	public function getPoi() { return $this->poi; }
+	public function getPoiSingle() { return $this->poi_single; }
+
+	public function create() {
+		// Crea le directory della struttura
+		foreach ($this->all_dir as $dir) {
+			if(!file_exists($dir)) {
+				// TODO: test exception
+				if(!mkdir($dir)) {
+					throw new Exception("Error cant't create directory $dir", 1);
+				}
+			}
+		}
+	}
+
+	public function checkPoi() {
+		// Controlla la struttura per i POI (geojson, poi, poi_single)
+		if(!file_exists($this->geojson))
+			throw new Exception("Error: directory geojson does not exist - ".$this->geojson, 1);
+		if(!file_exists($this->poi))
+			throw new Exception("Error: directory poi does not exist - ".$this->poi, 1);
+		if(!file_exists($this->poi_single))
+			throw new Exception("Error: directory poi_single does not exist - ".$this->poi_single, 1);
+		return true;
+	}
+
+
 }
--- tests/WebmappProjectStructureTest.php
+++ tests/WebmappProjectStructureTest.php
@@ -3,4 +3,64 @@ use PHPUnit\Framework\TestCase;
 
 class WebmappProjectStructureTest extends TestCase { 
 
+    private $root = '/tmp/testWebmapp/';
+    private $geojson = '/tmp/testWebmapp/geojson/';
+    private $poi = '/tmp/testWebmapp/geojson/poi/';	
+	private $poi_single = '/tmp/testWebmapp/geojson/poi/id/';
+
+	public function testOk() {
+
+
+        $s = new WebmappProjectStructure($this->root);
+
+        $s->create();
+
+        $this->assertEquals($this->root,$s->getRoot());
+        $this->assertEquals($this->geojson,$s->getGeojson());
+        $this->assertEquals($this->poi,$s->getPoi());
+        $this->assertEquals($this->poi_single,$s->getPoiSingle());
+        $this->assertTrue(file_exists($this->root));
+        $this->assertTrue(file_exists($this->geojson));
+        $this->assertTrue(file_exists($this->poi));
+        $this->assertTrue(file_exists($this->poi_single));
+
+        $this->assertTrue($s->checkPoi());
+
+	}
+
+	public static function delTree($dir) { 
+		$files = array_diff(scandir($dir), array('.','..'));
+		foreach ($files as $file) { 
+			(is_dir("$dir/$file")) ? self::delTree("$dir/$file") : unlink("$dir/$file");
+		}
+		return rmdir($dir);
+	}
+
+	public function testCheckPoiNoPoiSingle() {
+		$s = new WebmappProjectStructure($this->root);
+		$s->create();
+		self::delTree($this->poi_single);
+		// TODO: controllare lo specifico messaggio di errore con il metodo expectExceptionMessage ( che non funziona )
+		// $this->expectExceptionMessage('Error: directory poi_single does not exist');
+		$this->expectException(Exception::class);
+		$s->checkPoi();
+	}
+
+	public function testCheckPoiNoPoi() {
+		$s = new WebmappProjectStructure($this->root);
+		$s->create();
+		self::delTree($this->poi);
+		// TODO: controllare lo specifico messaggio di errore con il metodo expectExceptionMessage ( che non funziona )
+		$this->expectException(Exception::class);
+		$s->checkPoi();
+	}
+
+	public function testCheckPoiNoGeojson() {
+		$s = new WebmappProjectStructure($this->root);
+		$s->create();
+		self::delTree($this->geojson);
+		// TODO: controllare lo specifico messaggio di errore con il metodo expectExceptionMessage ( che non funziona )
+		$this->expectException(Exception::class);
+		$s->checkPoi();
+	}
 }
