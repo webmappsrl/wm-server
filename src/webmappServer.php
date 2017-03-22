f8ec29502a167fd24cc62a3baf58ab85e44928dc
piccioli <piccioli@netseven.it>
2017-03-22 07:24:10 +0100

    Aggiunto parametro allo script eseguibile

--- src/webmappServer.php
+++ src/webmappServer.php
@@ -1,5 +1,8 @@
 <?php
 require('autoload.php');
 // $root = __DIR__.'/../data/api.webmapp.it/example.webmapp.it/';
-// $s = new WebmappProjectStructure($root);
+$root = $argv[1];
+$s = new WebmappProject($root);
+$s->check();
+$s->process();
 
