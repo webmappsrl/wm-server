92654d2c0a86278f911a66e5680e6c62926af25a
piccioli <piccioli@netseven.it>
2017-03-22 11:15:45 +0100

    Aggiunti comandi osm2pgsql

--- src/classes/WebmappOSMListTask.php
+++ src/classes/WebmappOSMListTask.php
@@ -21,21 +21,45 @@ class WebmappOSMListTask extends WebmappAbstractTask {
 	}
 
     public function process(){
+
+        // TODO: passare ad un sistema di LOG
+        echo "Processing task: {$this->getName()}\n\n";
+
     	$path_osm = $this->root.'/server/osm';
     	if(!file_exists($path_osm)) mkdir ($path_osm);
     	$urls = file($this->getPathList(),FILE_IGNORE_NEW_LINES);
+        $first = true;
     	foreach($urls as $url) {
+            echo "Processing URL: $url\n";
+
             // NODE https://www.openstreetmap.org/node/353049774
             // WAY https://www.openstreetmap.org/way/284449666
     		// RELATION https://www.openstreetmap.org/relation/4174475
     		$components = explode('/', $url);
     		$type = $components[3];
     		$id = $components[4];
-    		file_put_contents($path_osm.'/'.$id.'.'.$type.'.osm', file_get_contents($this->getOverpassUrl($type,$id)));
+            $file_osm = $path_osm.'/'.$id.'.'.$type.'.osm';
 
-  			// TODO: osm2pgsql
+    		file_put_contents($file_osm, file_get_contents($this->getOverpassUrl($type,$id)));
+
+            // TODO: Generalizzare questa parte inserendo in un file di configurazione
+            //       i parametri di connessione per pgsql
+
+            // OSM2SQL - Parte funzionante solo sul server mappalo
+            // osm2pgsql -c -d gis -H localhost -U webmapp rifugi.osm --style /var/www/mappalo-server/openstreetmap-carto.style
+            $option='--append';
+            if ($first) {
+                $first = FALSE;
+                $option = '-c';
+            }
+
+            $cmd = "osm2pgsql $option -d gis -H localhost -U $file_osm --style /var/www/mappalo-server/openstreetmap-carto.style"; 
+            // TODO: passare ad un sistema di LOG
+            echo "$cmd \n";
+            system($cmd);
 
   			// TODO: ogr2ogr per la creazione dei file geojson
+
 
     	}
     	return TRUE;
@@ -45,7 +69,6 @@ class WebmappOSMListTask extends WebmappAbstractTask {
     private function getOverpassUrl($type,$id) {
 $overpass_query = <<<EOF
 [out:xml][timeout:1200];
-// gather results
 $type($id);
 (._;>;);
 out meta;
