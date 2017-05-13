f5ccfa36065fb775eb65e8e00718f2e4e990e20b
piccioli <piccioli@netseven.it>
2017-05-13 12:35:37 +0200

    Aggiunta la classe layer

---
+++ src/classes/WebmappLayer.php
@@ -0,0 +1,5 @@
+<?php // WebmappLayer.php
+
+class WebmappLayer {
+	
+}
---
+++ tests/WebmappLayerTest.php
@@ -0,0 +1,9 @@
+<?php // WebmappLayerTest.php
+
+use PHPUnit\Framework\TestCase;
+
+class WebmappLayerTest extends TestCase {
+	public function testOk() {
+      $this->assertTrue(false);
+	}
+}
