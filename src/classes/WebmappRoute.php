08297862a65c6c69eff3f8b6605129cb67be6119
piccioli <piccioli@netseven.it>
2017-06-25 12:01:22 +0200

    load tracks

--- src/classes/WebmappRoute.php
+++ src/classes/WebmappRoute.php
@@ -4,29 +4,39 @@ class WebmappRoute {
 
 	private $id;
 	private $title;
-	private $json_array;
+	private $json_array = array();
+	private $tracks = array();
+	private $base_url;
 
 	public function __construct ($array_or_url) {
-		if (!is_array($array_or_url)) {
-			// E' Un URL quindi leggo API WP e converto in array
-			$this->json_array = json_decode(file_get_contents($array_or_url),true);
-		}
-		else {
-			// TODO: implementare la lettura dell'array diretta
-			throw new Exception("Lettura diretta array ancora anon implementato", 1);
-			$this->json_array = $array_or_url;
-		}
-
+		$this -> base_url = preg_replace('|route/.*|', '', $array_or_url);
+		$this->json_array = json_decode(file_get_contents($array_or_url),true);
+		
 		$this->id = $this->json_array['id'];
 		$this->title = $this->json_array['title']['rendered'];
+		if (isset($this->json_array['n7webmap_route_related_track']) && 
+			count($this->json_array['n7webmap_route_related_track']) > 0 ) {
+			$this->loadTracks();
 	}
+}
 
-	public function getId() {
-		return $this->id ;
-	}
+public function getId() {
+	return $this->id ;
+}
 
-	public function getTitle() {
-		return $this->title ;
+public function getTitle() {
+	return $this->title ;
+}
+
+public function getTracks() {
+	return $this->tracks;
+}
+
+private function loadTracks() {
+	foreach ($this->json_array['n7webmap_route_related_track'] as $track ) {
+		$url = $this->base_url .'track/' . $track['ID'];
+		array_push($this->tracks, new WebmappTrackFeature($url));
 	}
+}
 
 }
--- tests/WebmappRouteTest.php
+++ tests/WebmappRouteTest.php
@@ -8,6 +8,11 @@ class WebmappRouteTest extends TestCase {
 		$r = new WebmappRoute('http://dev.be.webmapp.it/wp-json/wp/v2/route/346');
 		$this->assertEquals('346',$r->getId());
 		$this->assertEquals('Next to Net7 ROUTE',$r->getTitle());
+		$tracks = $r->getTracks();
+		$this->assertGreaterThan(0,count($tracks));
+		foreach ($tracks as $track) {
+			$this->assertEquals('WebmappTrackFeature',get_class($track));
+		}
 	}
 
 }
