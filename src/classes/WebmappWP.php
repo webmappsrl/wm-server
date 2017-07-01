a6ebc4e170d3acb629b4805c13d5455e27cc2416
piccioli <piccioli@netseven.it>
2017-07-01 09:28:33 +0200

    Aggiunte tutte le funzioni check per controllo esistenza URL / oggetti

--- src/classes/WebmappWP.php
+++ src/classes/WebmappWP.php
@@ -5,6 +5,7 @@ class WebmappWP {
 	private $base_url;
 	private $api_url;
 	private $api_pois;
+	private $api_tracks;
 	private $api_routes;
 	private $api_areas;
 	private $api_maps;
@@ -15,6 +16,7 @@ class WebmappWP {
 		$this->base_url = "http://$code.be.webmapp.it";
 		$this->api_url = "{$this->base_url}/wp-json/wp/v2";
 		$this->api_pois = "{$this->api_url}/poi";
+		$this->api_tracks = "{$this->api_url}/track";
 		$this->api_routes = "{$this->api_url}/route";
 		$this->api_areas = "{$this->api_url}/area";
 		$this->api_maps = "{$this->api_url}/map";
@@ -42,6 +44,10 @@ class WebmappWP {
 		return $this->api_pois;
 	}
 
+	public function getApiTracks() {
+		return $this->api_tracks;
+	}
+
 	public function getApiRoutes() {
 		return $this->api_routes;
 	}
@@ -63,6 +69,10 @@ class WebmappWP {
 		return $this->api_pois.'/'.$id;
 	}
 
+	public function getApiTrack($id) {
+		return $this->api_tracks.'/'.$id;
+	}
+
 	public function getApiRoute($id) {
 		return $this->api_routes.'/'.$id;
 	}
@@ -77,6 +87,43 @@ class WebmappWP {
 
 	public function getApiCategory($id) {
 		return $this->api_categories.'/'.$id;
+	}
+
+	// CONTROLLI DI RISPOSTA DALLA PIATTAFORMA
+
+	private function checkUrl($url) {
+		$a = get_headers($url);
+		$match = (preg_match('/200/', $a[0]));
+		if($match==0) return false;
+		return true;
+	}
+
+	public function check() {
+		return $this->checkUrl($this->base_url);
+	}
+
+	public function checkPoi($id) {
+		return $this->checkUrl($this->getApiPoi($id));
+	}
+
+	public function checkTrack($id) {
+		return $this->checkUrl($this->getApiTrack($id));
+	}
+
+	public function checkRoute($id) {
+		return $this->checkUrl($this->getApiRoute($id));
+	}
+
+	public function checkArea($id) {
+		return $this->checkUrl($this->getApiArea($id));
+	}
+
+	public function checkMap($id) {
+		return $this->checkUrl($this->getApiMap($id));
+	}
+
+	public function checkCategory($id) {
+		return $this->checkUrl($this->getApiCategory($id));
 	}
 
 }
--- tests/WebmappWPTest.php
+++ tests/WebmappWPTest.php
@@ -11,16 +11,33 @@ class WebmappWPTest extends TestCase
 		$this->assertEquals('http://test.be.webmapp.it/wp-json/wp/v2',$wp->getApiUrl());
 
 		$this->assertEquals('http://test.be.webmapp.it/wp-json/wp/v2/poi',$wp->getApiPois());
+		$this->assertEquals('http://test.be.webmapp.it/wp-json/wp/v2/track',$wp->getApiTracks());
 		$this->assertEquals('http://test.be.webmapp.it/wp-json/wp/v2/route',$wp->getApiRoutes());
 		$this->assertEquals('http://test.be.webmapp.it/wp-json/wp/v2/area',$wp->getApiAreas());
 		$this->assertEquals('http://test.be.webmapp.it/wp-json/wp/v2/map',$wp->getApiMaps());
 		$this->assertEquals('http://test.be.webmapp.it/wp-json/wp/v2/webmapp_category',$wp->getApiCategories());
 
 		$this->assertEquals('http://test.be.webmapp.it/wp-json/wp/v2/poi/10',$wp->getApiPoi(10));
+		$this->assertEquals('http://test.be.webmapp.it/wp-json/wp/v2/track/10',$wp->getApiTrack(10));
 		$this->assertEquals('http://test.be.webmapp.it/wp-json/wp/v2/route/10',$wp->getApiRoute(10));
 		$this->assertEquals('http://test.be.webmapp.it/wp-json/wp/v2/area/10',$wp->getApiArea(10));
 		$this->assertEquals('http://test.be.webmapp.it/wp-json/wp/v2/map/10',$wp->getApiMap(10));
 		$this->assertEquals('http://test.be.webmapp.it/wp-json/wp/v2/webmapp_category/10',$wp->getApiCategory(10));
+
+	}
+
+	public function testCheck() {
+		$wp = new WebmappWP('notvalid');
+		$this->assertFalse($wp->check());
+
+		$wp = new WebmappWP('dev');
+		$this->assertTrue($wp->check());
+		$this->assertTrue($wp->checkPoi(567));
+		$this->assertTrue($wp->checkTrack(606));
+		$this->assertTrue($wp->checkRoute(346));
+		// TODO: test area $this->assertTrue($wp->checkArea());
+		$this->assertTrue($wp->checkMap(414));
+		$this->assertTrue($wp->checkCategory(14));
 
 	}
 }
