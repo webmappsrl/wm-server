b646b36df9db913bc97e8d4c98acb7325ab01d76
piccioli <piccioli@netseven.it>
2017-07-13 16:05:50 +0200

    Refuso sulla classe

--- src/webmappServerCleanProject.php
+++ src/webmappServerCleanProject.php
@@ -2,5 +2,5 @@
 require('autoload.php');
 // $root = __DIR__.'/../data/api.webmapp.it/example.webmapp.it/';
 $root = $argv[1];
-$s = new WebmappProject($root);
+$s = new WebmappProjectStructure($root);
 $s->clean();
