1b3da743dd81adb71c908ce33603a5610928c0bd
piccioli <piccioli@netseven.it>
2017-10-22 18:46:40 +0200

    Aggiunto metodo getLanguages() per recuperare tutte le lingue di una route

--- src/classes/WebmappRoute.php
+++ src/classes/WebmappRoute.php
@@ -8,10 +8,24 @@ class WebmappRoute {
 	private $tracks = array();
 	private $base_url;
 
+    // Array con le lingue presenti
+	private $languages = array();
+
 	public function __construct ($array_or_url) {
 		$this -> base_url = preg_replace('|route/.*|', '', $array_or_url);
 		$this->json_array = json_decode(file_get_contents($array_or_url),true);
 		
+
+		if (isset($this->json_array['wpml_translations']) && 
+			is_array($this->json_array['wpml_translations']) &&
+			count($this->json_array['wpml_translations'])>0) {
+			foreach($this->json_array['wpml_translations'] as $t ) {
+				$lang = $t['locale'];
+				array_push($this->languages, $lang);
+			}
+		}
+
+
 		$this->id = $this->json_array['id'];
 		$this->title = $this->json_array['title']['rendered'];
 		if (isset($this->json_array['n7webmap_route_related_track']) && 
@@ -30,6 +44,10 @@ public function getTitle() {
 
 public function getTracks() {
 	return $this->tracks;
+}
+
+public function getLanguages() {
+	return $this->languages;
 }
 
 private function loadTracks() {
