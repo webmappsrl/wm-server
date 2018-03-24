2325a07e52c388914536ea696f3dc94575afef9b
piccioli <piccioli@netseven.it>
2018-03-24 10:10:22 +0100

    Tolto template HTML da codice. TODO: passare ad un template engine

--- src/classes/caiosm/WebmappOSMCAIRelationsTask.php
+++ src/classes/caiosm/WebmappOSMCAIRelationsTask.php
@@ -212,70 +212,9 @@ class WebmappOSMCAIRelationsTask extends WebmappAbstractTask {
         $title = 'Catasto - ' . $this->name;
         $zip = '/resources/' . $this->name . '_all_gpx.zip';
         $shp = '/resources/' . $this->name . '_all_shp.zip';
-        $html = <<<EOF
-<!DOCTYPE html>
-<html>
-<head>
-<title>$title</title>
-<link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css">
-<style>
-table {
-    font-family: arial, sans-serif;
-    border-collapse: collapse;
-    width: 100%;
-}
 
-td, th {
-    border: 1px solid #dddddd;
-    text-align: left;
-    padding: 8px;
-}
+        include('home.html.php');
 
-tr:nth-child(even) {
-    background-color: #dddddd;
-}
-td.red {
-    background-color: #FF0000;
-}
-</style>
-
-<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
-<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
-<script>
-$(document).ready(function() {
-    $('#catasto').DataTable();
-} );
-</script>
-</head>
-<body>
-
-<h1>$title</h1>
-
-<p>
-  <a href="$zip">Download all GPX (created by WMT)</a>
-  <a href="$shp">Download SHP file</a>
-</p>
-
-<table id="catasto" >
-$thead
-<tbody>
- $rows
-</tbody>
-
-</table>
-
-<div id="footer">
-<p>
-  Questa pagina web &egrave; stata realizzata utilizzando i dati di <a href="http://openstreetmap.org">Openstreetmap</a>.<br />
-  I metadati utilizzati sono tag di OSM definiti nella <a href="http://wiki.openstreetmap.org/wiki/CAI">convenzione</a> tra OSM e il Club Alpino Italiano.<br />
-  Il software &egrave; stato realizzato da <a href="mailto:alessiopiccioli@webmapp.it">Alessio Piccioli</a> di <a href="http://webmapp.it">Webmapp s.r.l.</a>. 
-</p>
-</div>
-
-</body>
-</html>
-
-EOF;
 
 file_put_contents($this->project_structure->getRoot().'/index.html', $html);
     }
---
+++ src/classes/caiosm/home.html.php
@@ -0,0 +1,66 @@
+<?php
+
+$html = <<<EOF
+<!DOCTYPE html>
+<html>
+<head>
+<title>$title</title>
+<link href="https://cdn.datatables.net/1.10.16/css/jquery.dataTables.min.css" rel="stylesheet" type="text/css">
+<style>
+table {
+    font-family: arial, sans-serif;
+    border-collapse: collapse;
+    width: 100%;
+}
+
+td, th {
+    border: 1px solid #dddddd;
+    text-align: left;
+    padding: 8px;
+}
+
+tr:nth-child(even) {
+    background-color: #dddddd;
+}
+td.red {
+    background-color: #FF0000;
+}
+</style>
+
+<script src="https://ajax.googleapis.com/ajax/libs/jquery/1.12.4/jquery.min.js"></script>
+<script src="https://cdn.datatables.net/1.10.16/js/jquery.dataTables.min.js"></script>
+<script>
+$(document).ready(function() {
+    $('#catasto').DataTable();
+} );
+</script>
+</head>
+<body>
+
+<h1>$title</h1>
+
+<p>
+  <a href="$zip">Download all GPX (created by WMT)</a>
+  <a href="$shp">Download SHP file</a>
+</p>
+
+<table id="catasto" >
+$thead
+<tbody>
+ $rows
+</tbody>
+
+</table>
+
+<div id="footer">
+<p>
+  Questa pagina web &egrave; stata realizzata utilizzando i dati di <a href="http://openstreetmap.org">Openstreetmap</a>.<br />
+  I metadati utilizzati sono tag di OSM definiti nella <a href="http://wiki.openstreetmap.org/wiki/CAI">convenzione</a> tra OSM e il Club Alpino Italiano.<br />
+  Il software &egrave; stato realizzato da <a href="mailto:alessiopiccioli@webmapp.it">Alessio Piccioli</a> di <a href="http://webmapp.it">Webmapp s.r.l.</a>. 
+</p>
+</div>
+
+</body>
+</html>
+
+EOF;
