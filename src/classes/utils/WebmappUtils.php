c0462a708131ef25e82cd89d2b71978514c0ae74
piccioli <piccioli@netseven.it>
2018-07-05 10:38:16 +0200

    Rimessa chiamata x cURL (x vetrina Toscana) abilitando l'opzione redirect per gli URL in lingua.

--- src/classes/utils/WebmappUtils.php
+++ src/classes/utils/WebmappUtils.php
@@ -279,7 +279,16 @@ class WebmappUtils {
 			}
 		}
 		if ($download) {
-			$output=file_get_contents($url);
+			// $output=file_get_contents($url);
+
+			$ch = curl_init();
+			curl_setopt($ch, CURLOPT_URL, $url);
+			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+			curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+			$output = curl_exec($ch);
+			curl_close($ch);
+
 			if ($webcache) {
 				// Write on DB
 				$q="INSERT into webcache (url,content,timestamp) VALUES (:url,:content,:time)";
