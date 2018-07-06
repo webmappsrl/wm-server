fef1027b8ee6773a674d8bbc1086ddef764bbad8
piccioli <piccioli@netseven.it>
2018-07-06 18:35:33 +0200

    BUGFIX: switch file / risorsa web

--- src/classes/utils/WebmappUtils.php
+++ src/classes/utils/WebmappUtils.php
@@ -279,15 +279,19 @@ class WebmappUtils {
 			}
 		}
 		if ($download) {
-			// $output=file_get_contents($url);
 
-			$ch = curl_init();
-			curl_setopt($ch, CURLOPT_URL, $url);
-			curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
-			curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
-			curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
-			$output = curl_exec($ch);
-			curl_close($ch);
+			// switch file / URL
+			if(preg_match('/^http/',$url)) {
+				$ch = curl_init();
+				curl_setopt($ch, CURLOPT_URL, $url);
+				curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
+				curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+				curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
+				$output = curl_exec($ch);
+				curl_close($ch);				
+			} else {
+			 	$output=file_get_contents($url);				
+			}
 
 			if ($webcache) {
 				// Write on DB
