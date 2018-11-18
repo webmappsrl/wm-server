575403b4361cf5cfdb974fb167f6cb596224664a
piccioli <piccioli@netseven.it>
2018-11-18 22:01:30 +0100

    Creata classe costo pe iil task SIMap (Sentiero Italia)

--- src/autoload.php
+++ src/autoload.php
@@ -64,3 +64,4 @@ require 'classes/custom/WebmappAlaTask.php';
 require 'classes/custom/WebmappEmpTask.php';
 require 'classes/custom/WebmappPfEventsTask.php';
 require 'classes/custom/WebmappTDPTask.php';
+require 'classes/custom/WebmappSIMapTask.php';
---
+++ src/classes/custom/WebmappSIMapTask.php
@@ -0,0 +1,14 @@
+<?php
+// Task per la realizzazione della mappa interattiva
+// del Sentiero Italia (SIMAP)
+class WebmappSIMapTask extends WebmappAbstractTask {
+
+	public function check() {
+        return TRUE;
+    }
+
+    public function process() {
+        return TRUE;
+    }
+
+}
