53b85ca107cf68e344903ae8b8b88846a2980c01
piccioli <piccioli@netseven.it>
2019-04-01 17:34:32 +0200

    BUG FIX update also all-pois.gojson

--- src/classes/task/WebmappAddTermNameToFeaturesTask.php
+++ src/classes/task/WebmappAddTermNameToFeaturesTask.php
@@ -26,21 +26,41 @@ class WebmappAddTermNameToFeaturesTask extends WebmappAbstractTask {
      $this->taxonomies[$name]=WebmappUtils::getJsonFromApi($this->getRoot().'/taxonomies/'.$name.'.json');
    }
    private function addTerm($name) {
+    $names = array();
     if(isset($this->taxonomies[$name]) && count($this->taxonomies[$name])>0) {
         foreach($this->taxonomies[$name] as $term_id => $term) {
-            $name = $term['name'];
+            $term_name = $term['name'];
             echo "Checking TERM $name($term_id)\n";
             if(isset($term['items']['poi']) && count($term['items']['poi'])>0) {
                 foreach ($term['items']['poi'] as $pid ) {
                     $poi = WebmappUtils::getJsonFromApi($this->getRoot().'/geojson/'.$pid.'.geojson');
                     $poi_name = '';
                     if(isset($poi['properties']['name'])) $poi_name = $poi['properties']['name'];
-                    $poi['properties']['name'] = ucfirst($name) .' - '.$poi_name;
+                    $poi['properties']['name'] = ucfirst($term_name) .' - '.$poi_name;
+                    $names[$pid]=ucfirst($term_name) .' - '.$poi_name;
                     echo "--> Adding term name to POI {$poi['properties']['name']} ($pid)\n";
                     file_put_contents($this->getRoot().'/geojson/'.$pid.'.geojson',json_encode($poi));
                 }
             }
         }
+        // UPDATE all_pois.gojson
+        if($name=='webmapp_category' && count($names)>0){
+          $file = $this->getRoot().'/geojson/all-pois.geojson';
+          $all = WebmappUtils::getJsonFromApi($file);
+          $all_new = array();
+          if(isset($all['features']) && count($all['features'])>0) {
+            foreach($all['features'] as $item) {
+              if(isset($names[$item['properties']['id']])){
+                $item['properties']['name']=$names[$item['properties']['id']];
+              }
+              $all_new['features'][]=$item;
+            }
+            $all_new['type']=$all['type'];
+            if(isset($all['properties'])) $all_new['properties']=$all['properties'];
+            file_put_contents($file,json_encode($all_new));
+          }
+        }
+
     }
    }
 
