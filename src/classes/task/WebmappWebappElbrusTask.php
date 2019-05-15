bd4a3b445877a59392162cf15b9a45f0fd7117b7
Davide Pizzato <davidepizzato@webmapp.it>
2019-05-15 12:48:49 +0200

    Completed check

--- src/classes/task/WebmappWebappElbrusTask.php
+++ src/classes/task/WebmappWebappElbrusTask.php
@@ -9,9 +9,25 @@ class WebmappWebappElbrusTask extends WebmappAbstractTask
             throw new Exception("ERRORE: Core mancante in '{$this->project_structure->getRoot()}/core.zip'", 1);
         }
 
+        if (!array_key_exists('base_url', $this->options)) {
+            throw new Exception("'base_url' option is mandatory", 1);
+        }
+
         if (!array_key_exists('codes', $this->options)) {
             throw new Exception("'codes' option is mandatory", 1);
         }
+
+        $codes = '';
+
+        foreach ($this->options['codes'] as $code) {
+            if ($codes != '') {
+                $codes .= ', ';
+            }
+
+            $codes .= $code;
+        }
+
+        echo "\nVerranno rigenerate le webapp per {$codes} nella root directory " . $this . $options['base_rl'] . "\n";
 
         return true;
     }
