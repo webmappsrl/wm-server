4b227a23f29ba6f907aef118ca924ea6d1585063
Davide Pizzato <davidepizzato@webmapp.it>
2019-05-15 12:34:02 +0200

    Project base url taken dynamically

--- src/classes/task/WebmappWebappElbrusTask.php
+++ src/classes/task/WebmappWebappElbrusTask.php
@@ -1,13 +1,12 @@
 <?php
 class WebmappWebappElbrusTask extends WebmappAbstractTask
 {
-    private $base_url = '/var/www/html/k.webmapp.it/';
     private $codebase_url = 'wm-webapp/';
 
     public function check()
     {
-        if (!file_exists($this->$base_url . 'core.zip')) {
-            throw new Exception("ERRORE: Core mancante in {$this->$base_url} . 'core.zip'", 1);
+        if (!file_exists($this->project_structure->getRoot() . 'core.zip')) {
+            throw new Exception("ERRORE: Core mancante in {$this->project_structure->getRoot()} . 'core.zip'", 1);
         }
 
         if (!array_key_exists('codes', $this->options)) {
