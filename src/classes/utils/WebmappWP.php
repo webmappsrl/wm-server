02f2b23eea6a9a6d81d05c3d32e808bf7acc4289
piccioli <piccioli@netseven.it>
2019-08-18 10:33:29 +0200

    Aggiunto parametro NO cache nel controllo degli URL

--- src/classes/utils/WebmappWP.php
+++ src/classes/utils/WebmappWP.php
@@ -252,6 +252,7 @@ class WebmappWP {
 		curl_setopt($ch, CURLOPT_URL, $url);
 		curl_setopt($ch, CURLOPT_RETURNTRANSFER, 1);
 		curl_setopt($ch, CURLOPT_FOLLOWLOCATION, true);
+		curl_setopt($ch, CURLOPT_FRESH_CONNECT, TRUE);
 		curl_setopt($ch,CURLOPT_USERAGENT,'Mozilla/5.0 (Windows; U; Windows NT 5.1; en-US; rv:1.8.1.13) Gecko/20080311 Firefox/2.0.0.13');
 
 		// this function is called by curl for each header received
