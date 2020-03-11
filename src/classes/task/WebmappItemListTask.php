32a08c1052321f47492f3875ced79c2ed80d26e0
piccioli <piccioli@netseven.it>
2020-03-11 18:49:57 +0100

    Added all properties and write to file.

--- src/classes/task/WebmappItemListTask.php
+++ src/classes/task/WebmappItemListTask.php
@@ -4,9 +4,13 @@ class WebmappItemListTask extends WebmappAbstractTask {
 
  public function check() {
 
-     // API Url for items
-     if(!array_key_exists('url', $this->options))
+     // ENDPOINT API Url for items
+     if(!array_key_exists('endpoint', $this->options))
          throw new Exception("'url' option is mandatory", 1);
+
+     // Name of the item to be downloaded: API will be endpoint.'/'.name
+     if(!array_key_exists('name', $this->options))
+         throw new Exception("'name' option is mandatory", 1);
 
      // Output file name
      if(!array_key_exists('out', $this->options))
@@ -17,9 +21,48 @@ class WebmappItemListTask extends WebmappAbstractTask {
 
 
 public function process(){
-    echo "Hello Process OK!";
-    return true;
 
+     // Retrieve items
+    $items = WebmappUtils::getMultipleJsonFromApi($this->options['endpoint'].'/'.$this->options['name']);
+    // Loop on items and build features array
+    $features = array();
+    if (is_array($items) && count($items)>0) {
+        foreach ($items as $item) {
+            $props=array();
+            $id = $item['id'];
+            $name = $item['title']['rendered'];
+            echo "processing $id $name\n";
+
+            $image = '';
+            $image_id = $item['featured_media'];
+            if (!empty($image_id)) {
+                $j = WebmappUtils::getJsonFromApi($this->options['endpoint'].'/media/'.$image_id);
+                if (isset($j['media_details']['sizes']['medium']['source_url']))
+                    $image = $j['media_details']['sizes']['medium']['source_url'];
+            }
+            $in = $item['excerpt']['rendered'];
+            $in = preg_replace('|\[|', '<', $in);
+            $in = preg_replace('|\]|', '>', $in);
+            $excerpt = strip_tags($in);
+
+            $web = $item['link'];
+
+            $props['id']=$id;
+            $props['name']=$name;
+            if(!empty($image)) $props['image']=$image;
+            $props['excerpt']=$excerpt;
+            $props['web']=$web;
+
+            $feature = array();
+            $feature['properties']=$props;
+            $features[]=$feature;
+        }
+    }
+
+    // Write json file in resources
+    $json = array('type'=>'featureCollection','features'=>$features);
+    file_put_contents($this->getRoot().'/resources/'.$this->options['name'].'.json',json_encode($json));
+    return true;
 }
 
 }
