c038814f774f504513efa901168fa7f0d1e267a1
Davide Pizzato <davidepizzato@webmapp.it>
2020-10-16 12:48:16 +0200

    Updated version number

--- src/wmcli.php
+++ src/wmcli.php
@@ -2,9 +2,9 @@
 /**
  * Project: wm-server
  * Author: Webmapp
- * Version 0.1.7
+ * Version 0.1.8
  */
-echo "wmcli v0.1.7\n";
+echo "wmcli v0.1.8\n";
 
 require 'autoload.php';
 $c = new WebmappCli($argv);
